51fa5c614d1ffc030b4aa4b551f1993c9f7628b6
fernandocruz184 <id8006@alunos.uminho.pt>
2022-05-22 16:21:27 +0100

    [ADD] adding favicon and manifest

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {Box, Grid, Typography} from "@mui/material";
+import CodeSharpIcon from '@mui/icons-material/CodeSharp';
 
 function App() {
     return (
@@ -16,7 +17,7 @@ function App() {
                   justifyContent="center"
                   alignItems="center">
                 <Grid item>
-                    <Typography color={"primary.main"} variant="h2" component="h2">
+                    <Typography mb={2} color={"primary.main"} variant="h2" component="h2">
                         IN CONSTRUCTION
                     </Typography>
                 </Grid>
