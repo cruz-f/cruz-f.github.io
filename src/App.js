424cd5c8a88d3f597b4a8809dbedb9284d33baad
fernandocruz184 <id8006@alunos.uminho.pt>
2022-06-25 13:57:17 +0100

    [ADD] add the home page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import * as React from 'react';
-import {Box, Grid, Typography} from "@mui/material";
-import CodeSharpIcon from '@mui/icons-material/CodeSharp';
+import Home from "./sections/Home";
+
 
 function App() {
     return (
-        <Box sx={{
-            backgroundColor: 'text.secondary',
-            minHeight: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-        }}>
-            <Grid container
-                  direction="row"
-                  justifyContent="center"
-                  alignItems="center">
-                <Grid item>
-                    <Typography mb={2} color={"primary.main"} variant="h2" component="h2">
-                        IN CONSTRUCTION
-                    </Typography>
-                </Grid>
-            </Grid>
-        </Box>
+        <Home/>
     );
 }
 
---
+++ src/sections/Home.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import {Grid, Typography, Stack, IconButton, Box} from "@mui/material";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import ArrowDownwardSharpIcon from '@mui/icons-material/ArrowDownwardSharp';
+import AppDrawer from "../components/AppDrawer";
+
+
+function Home() {
+    return (
+        <Grid container
+              direction="row"
+              sx={{
+                  backgroundColor: '#212529',
+                  minHeight: '100vh',
+                  paddingX: '2rem',
+              }}
+              justifyContent="space-between"
+              alignItems="center">
+            <Grid item mt={5} xs={10} alignSelf={'flex-start'}>
+                <Typography
+                    color={'#FFF'}
+                    variant="h6"
+                    component="h6"
+                    sx={{
+                        fontWeight: 'lighter',
+                        textTransform: "uppercase",
+                        textAlign: 'justify',
+                    }}>
+                    Fernando Cruz
+                </Typography>
+            </Grid>
+            <Grid item mt={5} xs={"auto"} alignSelf={'flex-start'}>
+                <AppDrawer/>
+            </Grid>
+            <Grid item xs={6}>
+            </Grid>
+            <Grid item xs={4}>
+                <Typography
+                    color={'#FFF'}
+                    variant="h4"
+                    component="h4"
+                    sx={{
+                        fontWeight: 'lighter',
+                        textTransform: "uppercase",
+                        textAlign: 'justify',
+                    }}>
+                    Artificial Intelligence and Data Science for Biology
+                </Typography>
+                <Typography
+                    color={'#FFF'}
+                    variant="subtitle1"
+                    component="h3"
+                    sx={{
+                        paddingTop: ".8rem",
+                        textTransform: 'uppercase',
+                        textAlign: 'justify',
+                    }}>
+                    Hi there!
+                </Typography>
+                <Typography
+                    color={'#FFF'}
+                    variant="subtitle1"
+                    component="h3"
+                    sx={{
+                        fontWeight: 'lighter',
+                        textAlign: 'justify',
+                    }}>
+                    I am Fernando Cruz from Barcelos, Portugal.
+                    I am a Ph.D. student in Biomedical Engineering and Bioinformatics
+                    at the University of Minho.
+                </Typography>
+            </Grid>
+            <Grid item xs={2}>
+            </Grid>
+            <Grid item xs={10} mb={3} alignSelf={'flex-end'}>
+                <IconButton href={"https://www.linkedin.com/in/fernando-cruz-ba2b65142"}>
+                    <LinkedInIcon sx={{ color: "#FFF" }} fontSize="large"/>
+                </IconButton>
+                <IconButton href={"https://github.com/cruz-f"}>
+                    <GitHubIcon sx={{ color: "#FFF" }} fontSize="large"/>
+                </IconButton>
+                <IconButton href={"mailto:fernando.cruz@ceb.uminho.pt"}>
+                    <AlternateEmailIcon sx={{ color: "#FFF" }} fontSize="large"/>
+                </IconButton>
+            </Grid>
+            <Grid item xs={'auto'} mb={3} alignSelf={'flex-end'}>
+                {/*<IconButton href={"/"}>
+                    <ArrowDownwardSharpIcon sx={{ color: "#FFF" }} fontSize="large"/>
+                </IconButton>*/}
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Home;
