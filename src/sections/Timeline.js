1497331ee54ebdba70d3cbfacc05180f619dc63b
fernandocruz184 <id8006@alunos.uminho.pt>
2022-11-06 20:33:04 +0000

    [FIX] minor issue

--- src/sections/Timeline.js
+++ src/sections/Timeline.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {useState} from "react";
 import Typography from '@mui/material/Typography';
 import {Grid} from "@mui/material";
 import Timeline from '@mui/lab/Timeline';
